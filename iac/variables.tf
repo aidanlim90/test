ad1d948b31580334811b7e6435ae51dba48f121f
Lim Yoong Wah <wag900813@outlook.com>
2022-08-03 17:24:13 +0800

    test1

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,7 +1,7 @@
 #Azure account variables
 variable "sub" {
   type        = string
-  description = "Subscription that will host the infrastructure 1 2"
+  description = "Subscription that will host the infrastructure 1 2 3"
 }
 
 variable "client_secret" {
